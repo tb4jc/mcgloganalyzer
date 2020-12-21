0c224dbb65cc38af7ff12c4b94e700b85c45461e
Thomas Burri <th@thonjato.ch>
2020-12-21 11:16:57 +0100

    Updated directory name to camel case again. Added unpack_archive.cmd batch file

---
+++ unpack_archive.cmd
@@ -0,0 +1,15 @@
+@echo off
+set CWD=%~dp0
+
+if not exist %CWD%set_python_path.cmd (
+    color 47
+    echo.
+    echo "Create file 'set_python_path.cmd' that sets environment variable PYTHON_PATH!"
+    pause
+    exit /b
+)
+
+call %CWD%set_python_path.cmd
+
+%PYTHON_PATH%\python.exe %CWD%mcgLogAnalyzer.py --unpack-only %1
+timeout 5
